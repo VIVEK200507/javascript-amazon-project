3fac090324e644bdbfa412f844760a3f2f5b3e95
VIVEK200507 <141722359+VIVEK200507@users.noreply.github.com>
2024-04-22 18:20:58 +0530

    delete product from card

--- script/checkout.js
+++ script/checkout.js
@@ -8,7 +8,7 @@ cart.forEach((cardItem)=>{
         }
         
     })
-        CardSummryHTML+=`<div class="cart-item-container">
+        CardSummryHTML+=`<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
         Delivery date: Wednesday, June 15
         </div>
@@ -89,5 +89,8 @@ document.querySelectorAll('.js-delete-quantity-link').forEach((link)=>{
         const productId=link.dataset.productId;
         removeFromCart(productId);
         console.log(cart);
+        const container=document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     });
+   
 });
