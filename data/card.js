10fd3d7af424cb47288b85e2aa0000a049645c72
VIVEK200507 <141722359+VIVEK200507@users.noreply.github.com>
2024-04-22 18:31:33 +0530

    save cart local storage

--- data/card.js
+++ data/card.js
@@ -1,4 +1,6 @@
-let cart=[{
+let cart=JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart=[{
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2
 },
@@ -6,7 +8,11 @@ let cart=[{
     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity:1
 }];
+}
 
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 function addToCart(productId){ 
     let matchingItem;
@@ -24,6 +30,7 @@ function addToCart(productId){
         quantity:Math.floor(document.querySelector(`.js-quantity-selector-${productId}`).value)
       });
     }
+    saveToStorage();
 }
 
 function removeFromCart(productId){
@@ -35,4 +42,5 @@ function removeFromCart(productId){
     }
   });
   cart=newCart;
+  saveToStorage();
 }
