f3abaa7858eb601626330f304ae2c19a62de06dd
VIVEK200507 <141722359+VIVEK200507@users.noreply.github.com>
2024-04-29 22:53:09 +0530

    make delivery option interactive

--- script/checkout.js
+++ script/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart,updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -89,7 +89,9 @@ function deliveryOptionsHTML(matchingProduct,cartItem){
 
     const isChecked=deliveryOption.id===cartItem.deliveryOptionId;
     html+=`
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
               <input type="radio"
               ${isChecked?'checked':''}
                 class="delivery-option-input"
@@ -124,4 +126,11 @@ document.querySelectorAll('.js-delete-link')
       container.remove();
     });
 
+  });
+
+  document.querySelectorAll('.js-delivery-option').forEach((elemenet)=>{
+    elemenet.addEventListener('click',()=>{
+      const {productId,deliveryOptionId}=elemenet.dataset;
+      updateDeliveryOption(productId,deliveryOptionId)
+    });
   });
