5c43d91a64670f1f0eb415b3d4c8575b60f5e5f1
VIVEK200507 <141722359+VIVEK200507@users.noreply.github.com>
2024-06-07 01:15:38 +0530

    load products from the backend

--- script/checkout.js
+++ script/checkout.js
@@ -1,7 +1,10 @@
+import { loadProducts } from "../data/products.js";
 import { renderOrderSummry } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
-import '../data/backend-pratice.js';
+//import '../data/backend-pratice.js';
 
-renderOrderSummry();
-renderPaymentSummary();
+loadProducts(()=>{
+    renderOrderSummry();
+    renderPaymentSummary();
+});
